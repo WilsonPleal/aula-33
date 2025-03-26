f4961f07f91726fe84a46ecf6c65e814fc9a5eee
Wallace027Dev <wallaceofc@hotmail.com>
2025-03-25 21:40:35 -0300

    desafio concluido

--- api/src/controllers/controladorDeUsuario.js
+++ api/src/controllers/controladorDeUsuario.js
@@ -8,3 +8,57 @@ exports.obterTodosUsuarios = async (_req, res) => {
     res.status(500).send("Erro ao recuperar usuários");
   }
 };
+
+exports.obterUsuarioPeloID = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).send("ID não fornecido.");
+  }
+
+  try {
+    const usuario = await servicosDeUsuario.obterUsuarioPeloID(id);
+    if (!usuario) {
+      return res.status(404).send("Usuário não encontrado.");
+    }
+    res.json(usuario);
+  } catch (error) {
+    console.error(error); // Adiciona o erro no console
+    res.status(500).send("Erro ao recuperar usuário pelo ID");
+  }
+};
+
+exports.obterUsuarioPeloEmail = async (req, res) => {
+  const { email } = req.params;
+  if (!email) {
+    return res.status(400).send("Email não fornecido.");
+  }
+
+  try {
+    const usuario = await servicosDeUsuario.obterUsuarioPeloEmail(email);
+    if (!usuario) {
+      return res.status(404).send("Usuário não encontrado");
+    }
+    res.json(usuario);
+  } catch (error) {
+    console.error(error); // Adiciona o erro no console
+    res.status(500).send("Erro ao recuperar usuário pelo email");
+  }
+};
+
+exports.obterUsuarioPeloCpf = async (req, res) => {
+  const { cpf } = req.params;
+  if (!cpf) {
+    return res.status(400).send("CPF não fornecido.");
+  }
+
+  try {
+    const usuario = await servicosDeUsuario.obterUsuarioPeloCpf(cpf);
+    if (!usuario) {
+      return res.status(404).send("Usuário não encontrado");
+    }
+    res.json(usuario);
+  } catch (error) {
+    console.error(error); // Adiciona o erro no console
+    res.status(500).send("Erro ao recuperar usuário pelo CPF");
+  }
+};
--- api/src/repositories/repositorioDeUsuario.js
+++ api/src/repositories/repositorioDeUsuario.js
@@ -8,3 +8,15 @@ exports.encontrarTodosUsuarios = async () => {
   // Aqui você pode fazer a consulta real ao banco de dados
   return usuarios;
 };
+
+exports.encontrarPeloID = async (id) => {
+  return usuarios.find((usuario) => usuario.id === id);
+};
+
+exports.encontrarPeloEmail = async (email) => {
+  return usuarios.find((usuario) => usuario.email === email);
+};
+
+exports.encontrarPeloCpf = async (cpf) => {
+  return usuarios.find((usuario) => usuario.cpf === cpf);
+};
