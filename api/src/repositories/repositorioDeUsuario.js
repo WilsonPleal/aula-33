7838d1e235c840c3eca34852b509c63db000a059
Wallace027Dev <wallaceofc@hotmail.com>
2025-03-27 15:26:38 -0300

    novos objetos

--- api/src/repositories/repositorioDeUsuario.js
+++ api/src/repositories/repositorioDeUsuario.js
@@ -1,7 +1,18 @@
 const usuarios = [
-  { id: 1, nome: "Alice" },
-  { id: 2, nome: "Bob" },
-  { id: 3, nome: "Carol" }
+  {
+    "id": "925f1bac-e043-40c5-bac2-2ccab5ef92fe",
+    "nome": "Ada Lovelace",
+    "cpf": "12345678912",
+    "email": "adalovelace@email.com",
+    "senha": "$2b$10$RJ/qMPtPHPrv6P1Z7OjfweVYyhIfzg8uAHXfAdP5QvDxriK2SQu.K"
+  },
+  {
+    "id": "c0f7e291-bf51-4bb6-b844-fba97cfdc545",
+    "nome": "Alan Turing",
+    "cpf": "12345678912",
+    "email": "alanturing@email.com",
+    "senha": "$2b$10$ekjLiRuXwfIqNmCUcTTXH.UX1Xh8VJl48LPmBdE3MVgLHcCaF9JMW"
+  }
 ];
 
 exports.encontrarTodosUsuarios = async () => {
